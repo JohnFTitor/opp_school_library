e431b8d935960971e1557943706d54db27ef8cf7
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-04 17:00:40 -0500

    Create Person class

---
+++ person.rb
@@ -0,0 +1,29 @@
+class Person
+  attr_accessor :name
+  attr_accessor :age
+  attr_reader :id
+
+  def initialize(name = "Unknown", age, parent_permission: true)
+    @id = Random.rand(0..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
+  
+  private :is_of_age?
+
+  def can_use_services?
+    is_of_age? || @parent_permission
+  end
+end
+
+john_titor = Person.new('John Titor', 17, parent_permission: false)
+
+puts john_titor.name
+puts john_titor.age
+puts john_titor.id
+puts john_titor.can_use_services?
